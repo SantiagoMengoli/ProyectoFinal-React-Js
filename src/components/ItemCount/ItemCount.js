ac4e32a61ae0ab29f85c38678eb2b0b9cfae785b
SANTIAGO MENGOLI <santiagomengoli@gmail.com>
2021-04-09 12:28:59 -0300

    Comentario

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -11,7 +11,7 @@ export default class ItemCount extends Component {
       count: 0,
     };
   }
-
+//Comentario
   handleIncrement = () => {
     this.setState({ count: this.state.count + 1 });
   };
