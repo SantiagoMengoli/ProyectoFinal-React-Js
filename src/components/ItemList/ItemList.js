6617dd1d9c59a220822f842d776a8116489d1f12
SANTIAGO MENGOLI <santiagomengoli@gmail.com>
2021-05-18 18:28:35 -0300

    Loader agregado

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,51 +1,52 @@
+
 import React, { Component } from 'react';
 import { Item } from '../Item/Item';
 import Spinner from '../Spinner/Spinner'
 import './ItemList.css';
 
-class ItemList extends Component { 
-   constructor(props) {
-        super(props);
-        this.state = {
-            productoSeleccionado: []
-        };
-    }
 
-    state = {
-        loading : false
-    }
-    componentDidMount (){
-        setTimeout(() => {
-            this.setState ({loading: false}) 
-        },5000);
-    }
-   
-   /* componentDidMount(){
-        setTimeout(() => {
-            this.setState({
-                productoSeleccionado: this.props.items,
-            })
-        }, 2000);
-    }*/
-    componentDidUpdate(){
-        setTimeout(() => {
-            this.setState({
-                productoSeleccionado: this.props.items,
-            })
-        }, 2000);
-    }
-    render(){
-        const {loading} = this.state
-        return (
-            <div className="ItemList">
-               {loading ? <Spinner /> : null}
-            {
+class ItemList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productoSeleccionado: [],
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        loading: false,
+        productoSeleccionado: this.props.items,
+      })
+    }, 2500);
+  }
+
+
+  componentDidUpdate() {
+    setTimeout(() => {
+      this.setState({
+        productoSeleccionado: this.props.items,
+      })
+    }, 2000);
+  }
+  
+  render() {
+
+    return (
+      <div className="ItemList">
+        {
+          this.state.loading ? <Spinner />
+            : (
               this.state.productoSeleccionado && this.state.productoSeleccionado.map((producto) => {
                 return <Item item={producto} key={producto.id} />;
               })
-            }
-          </div>
-    )}
-   }
+            )
+        }
+      </div>
+    )
+  }
+}
 
 export default ItemList;
--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -3,12 +3,14 @@ import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react'
 
 const Spinner = () => (
   <div>
+   
+
     <Segment>
       <Dimmer active inverted>
-        <Loader inverted>Loading</Loader>
+        <Loader inverted>Cargando</Loader>
       </Dimmer>
 
-      <Image src='/images/wireframe/short-paragraph.png' />
+      <Image src='https://img2.freepng.es/20191023/exz/transparent-soccer-ball-soccer-clipart-png-transparent-azpng5db11576d60270.6520987315718864548766.jpg'  width="150px" />
     </Segment>
   </div>
 )
