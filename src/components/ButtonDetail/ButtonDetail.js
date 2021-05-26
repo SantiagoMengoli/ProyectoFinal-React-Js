a2e122fc490fcd72c8da4cff0a0cb69734bf8d03
SANTIAGO MENGOLI <santiagomengoli@gmail.com>
2021-05-26 20:42:42 -0300

    Correccion buton carrito

--- src/components/ButtonDetail/ButtonDetail.js
+++ src/components/ButtonDetail/ButtonDetail.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Button, Icon } from 'semantic-ui-react'
+import './ButonDetail.css'
 
 function ButtonExampleAnimated (props) {
   const {onAdd} = props;
   return ( <div>
     <Button onClick={onAdd} className="buttonCarrito" animated='vertical'>
-      <Button.Content hidden>Carrito</Button.Content>
+      <Button.Content hidden>Terminar Compra</Button.Content>
       <Button.Content visible>
-        <Icon name='shop' />
+        <Icon name ='shop' />
       </Button.Content>
     </Button>
   </div>)
--- src/components/CarritoDeCompras/CarritoDeCompras.js
+++ src/components/CarritoDeCompras/CarritoDeCompras.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import './CarritoDeCompras.css'
-import ButtonFinalizarCompra from '../ButtonFinalizarCompra/ButtonFinalizarCompra'
 
  const CarritoDeCompras = () => {
     return (
       <div>
         <h2 className="tituloCarritoCompras"> Carrito de Compras</h2>
-        <div className="finalizarCompra">
-          <ButtonFinalizarCompra/>
-        </div>
+        
       </div>
     )
 }
