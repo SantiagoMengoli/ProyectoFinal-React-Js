a42f93cbfab2bca8f6864ffdab16e026c3355571
SANTIAGO MENGOLI <santiagomengoli@gmail.com>
2021-05-27 19:09:49 -0300

    boton

--- src/components/ButtonFinalizarCompra/ButtonFinalizarCompra.js
+++ src/components/ButtonFinalizarCompra/ButtonFinalizarCompra.js
@@ -3,7 +3,7 @@ import { Button } from 'semantic-ui-react'
 
 const ButtonFinalizarCompra = () => (
   <div>
-   <Button primary>Comprar</Button>
+   <Button primary>Terminar Compra</Button>
   </div>
 )
 export default ButtonFinalizarCompra
